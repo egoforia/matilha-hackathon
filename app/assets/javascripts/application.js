0044eca6cd4a702f03eb6fce4a95d5fa167e328a
Amilton L. Paglia <amilton@matilhaestud.io>
2016-10-16 03:48:33 -0200

    merge

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,22 +1,7 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
-<<<<<<< HEAD
 //= require jquery-ui
-=======
 //= require bootstrap-sprockets
->>>>>>> 161f61475d95444903972974bd603a5effae6333
 //= require turbolinks
 //= require_tree .
 //= require form-builder.js
