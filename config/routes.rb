afd10005254c4c7e092e8a06d485116bd915f65d
Diego Haas Sanches <egoforia@gmail.com>
2016-10-16 04:29:42 -0200

    scaffold controller landing index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'landing/index'
+
   devise_for :lawyers, controllers: { registrations: 'lawyers/registrations' }
   resources :answers
 
@@ -24,7 +26,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'forms#index'
+  root 'landing#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
