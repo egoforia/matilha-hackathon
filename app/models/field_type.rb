7314be60d7015a2eefea06a71b0220b147b15126
Diego Haas Sanches <egoforia@gmail.com>
2016-10-16 11:57:24 -0200

    dinamizado tela de input de usuário

--- app/models/field_type.rb
+++ app/models/field_type.rb
@@ -1,2 +1,26 @@
 class FieldType < ActiveRecord::Base
+
+	def is_text?
+		self.title == 'text'
+	end
+
+	def is_select?
+		self.title == 'select'
+	end
+
+	def is_check?
+		self.title == 'check'
+	end
+
+	def is_radio?
+		self.title == 'radio'
+	end
+
+	def is_number?
+		self.title == 'number'
+	end
+
+	def is_upload?
+		self.title == 'upload'
+	end
 end
