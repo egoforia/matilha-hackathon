57ed72adc59d45e766ca75fd3411518b27a714ff
Diego Haas Sanches <egoforia@gmail.com>
2016-10-15 21:23:27 -0300

    devise initial configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :lawyers
   resources :answers
 
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161015235728) do
+ActiveRecord::Schema.define(version: 20161016001755) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "user_id",            limit: 4,     null: false
@@ -48,15 +48,26 @@ ActiveRecord::Schema.define(version: 20161015235728) do
   add_index "forms", ["lawyer_id"], name: "index_forms_on_lawyer_id", using: :btree
 
   create_table "lawyers", force: :cascade do |t|
-    t.string   "OAB",              limit: 255
-    t.string   "name",             limit: 255, null: false
-    t.string   "email",            limit: 255, null: false
-    t.integer  "field_of_work_id", limit: 4
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.string   "OAB",                    limit: 255
+    t.string   "name",                   limit: 255,              null: false
+    t.integer  "field_of_work_id",       limit: 4
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
   end
 
+  add_index "lawyers", ["email"], name: "index_lawyers_on_email", unique: true, using: :btree
   add_index "lawyers", ["field_of_work_id"], name: "index_lawyers_on_field_of_work_id", using: :btree
+  add_index "lawyers", ["reset_password_token"], name: "index_lawyers_on_reset_password_token", unique: true, using: :btree
 
   create_table "question_options", force: :cascade do |t|
     t.string   "title",       limit: 255, null: false
