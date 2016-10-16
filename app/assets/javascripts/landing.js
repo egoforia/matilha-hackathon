194b27da256141f5b1f6cda4aa6aaf36927ecc5c
Amilton L. Paglia <amilton@matilhaestud.io>
2016-10-16 08:24:20 -0200

    landing page final front done

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,2 +1,12 @@
 //= require jquery
 //= require jquery_ujs
+//= require image-scale.js
+
+
+$(document).on('ready', function() {
+  $(".hero__image img").imageScale({
+  	scale: 'best-fill',
+  	align: 'center',
+  	rescaleOnResize: true
+  });
+});
